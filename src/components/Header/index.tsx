2bfe174a5358c6d6037f6894ca0a7e1e623b03cd
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-11-10 21:38:00 -0300

    feat: Criando o componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link'
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a><img src="logo.svg" alt="logo" /></a>
+      </Link>
+    </header>
+  )
 }
