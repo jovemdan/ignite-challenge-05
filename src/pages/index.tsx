f992c1f3fe42d7aa5a7ff2551d8fe42fe35496f1
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-11-14 22:27:43 -0300

    feat: criando funcionalidade de carregar posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import commonStyles from '../styles/common.module.scss'
 import styles from './home.module.scss'
 import { useState } from 'react'
 import { ptBR } from 'date-fns/locale'
+import Head from 'next/head'
 
 interface Post {
   uid?: string
@@ -47,9 +48,45 @@ export default function Home({ postsPagination }: HomeProps) {
   })
 
   const [posts, setPosts] = useState<Post[]>(formattedPost)
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return
+    }
+
+    const postsResults = await fetch(`${nextPage}`).then(response => response.json())
+
+    setNextPage(postsResults.next_page)
+    setCurrentPage(postsResults.page)
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    })
+
+    setPosts([...posts, ...newPosts])
+  }
 
   return (
     <>
+      <Head>
+        <title>Home | spacetraveling.</title>
+      </Head>
       <main className={commonStyles.container}>
         <Header />
 
@@ -72,10 +109,13 @@ export default function Home({ postsPagination }: HomeProps) {
               </a>
             </Link>
           ))}
-
-          <button type="button">
-            Carregar mais posts
-          </button>
+          {
+            nextPage && (
+              <button type="button" onClick={handleNextPage}>
+                Carregar mais posts
+              </button>
+            )
+          }
         </div>
       </main>
     </>
