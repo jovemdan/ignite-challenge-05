1a6bbba0105aaf3a8285c80fa7b52265095d3eae
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2022-11-15 22:03:06 -0300

    fix: Ajustando caminho para página de post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home({ postsPagination }: HomeProps) {
 
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link href={`/posts/${post.uid}`} key={post.uid}>
+            <Link href={`/post/${post.uid}`} key={post.uid}>
               <a className={styles.post}>
                 <strong>{post.data.title}</strong>
                 <p>{post.data.subtitle}</p>
